51621a2541d8ac801f1462edf832b9668e3f2c29
anant0059 <2019uce0059@gmail.com>
2023-02-06 21:42:40 +0530

    adding the new item

--- to-do-list-v1/app.js
+++ to-do-list-v1/app.js
@@ -3,43 +3,65 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = [];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-    switch (currentDay){
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to: " + currentDay);
-    }
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
+
+    // var currentDay = today.getDay();
+    // var day = "";
+
+    // switch (currentDay){
+    //     case 0:
+    //         day = "Sunday";
+    //         break;
+    //     case 1:
+    //         day = "Monday";
+    //         break;
+    //     case 2:
+    //         day = "Tuesday";
+    //         break;
+    //     case 3:
+    //         day = "Wednesday";
+    //         break;
+    //     case 4:
+    //         day = "Thursday";
+    //         break;
+    //     case 5:
+    //         day = "Friday";
+    //         break;
+    //     case 6:
+    //         day = "Saturday";
+    //         break;
+    //     default:
+    //         console.log("Error: current day is equal to: " + currentDay);
+    // }
     
-    res.render('list', {kindOfDay: day});
+    res.render('list', {kindOfDay: day, newListItem: items});
     
     //res.send();
     //res.send("Hello");
+});
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+    // console.log(item);
 });
 
 app.listen(3000, function(){
