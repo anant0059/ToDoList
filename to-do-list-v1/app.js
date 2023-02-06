d401d3c5d39dfc0183ff1d91ee66fbbb1e0a4736
anant0059 <2019uce0059@gmail.com>
2023-02-06 15:08:27 +0530

    v1 ejs-init

--- to-do-list-v1/app.js
+++ to-do-list-v1/app.js
@@ -3,15 +3,42 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
     var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
 
-    if(today.getDay() === 6 || today.getDay() === 0){
-        res.send("Yay it's the weekend!");
+    switch (currentDay){
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: current day is equal to: " + currentDay);
     }
-    else{
-        res.send("Boo! I have to work!");
-    }
+    
+    res.render('list', {kindOfDay: day});
+    
+    //res.send();
     //res.send("Hello");
 });
 
