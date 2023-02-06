c9a5a20c678394ab1fe8b2270367bf6b725c9855
anant0059 <2019uce0059@gmail.com>
2023-02-06 22:20:41 +0530

    v1 css-added

--- to-do-list-v1/app.js
+++ to-do-list-v1/app.js
@@ -3,22 +3,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+let items = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     // var currentDay = today.getDay();
     // var day = "";
@@ -56,7 +57,7 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
 
     items.push(item);
 
